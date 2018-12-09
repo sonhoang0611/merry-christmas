364fec3267ec86247776c543585410f1d799fe11
sonhoang0611 <son.hoang@holistics.io>
2018-12-09 20:01:59 +0700

    Initialize structure project, add bulma

---
+++ src/containers/MerryChristmas.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container is-fluid">
+    Merry Christmas!!!
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'christmasContainer',
+};
+</script>
+
+
+<style scoped lang="scss">
+
+</style>
