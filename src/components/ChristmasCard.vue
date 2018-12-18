e51080fb51a1147319f48aeeb8b5a73a2cf1fb4b
sonhoang0611 <son.hoang@holistics.io>
2018-12-18 08:25:24 +0700

    Refactor ChristmasCard, add new images

--- src/components/ChristmasCard.vue
+++ src/components/ChristmasCard.vue
@@ -1,25 +1,25 @@
 <template>
-  <div class="card">
+  <div class="christmas-card card">
     <div class="card-image">
       <figure class="image is-4by3">
-        <img src="../assets/card1.jpg" alt="Placeholder image">
+        <img :src="card.img" :alt="card.title">
       </figure>
     </div>
     <div class="card-content">
       <div class="media">
         <div class="media-left">
           <figure class="image is-48x48">
-            <img src="../assets/card1.jpg" alt="Placeholder image">
+            <img :src="card.img" :alt="card.title">
           </figure>
         </div>
         <div class="media-content">
-          <p class="title is-4">Ông già Noel</p>
-          <p class="subtitle is-6">@noel</p>
+          <p class="title is-4">{{card.title}}</p>
+          <p class="subtitle is-6">{{card.subtitle}}</p>
         </div>
       </div>
 
       <div class="content">
-        Merry Christmas. <a>@son.ht</a>.
+        {{card.content}} <a>@son.ht</a>.
         <a href="#">#snow</a> <a href="#">#winter</a>
         <br>
         <time datetime="2016-1-1">10:07 PM - 25 Dec 2018</time>
@@ -31,9 +31,16 @@
 <script>
 export default {
   name: 'christmasCard',
+  props: {
+    card: {
+      type: Object,
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-
+  .christmas-card {
+    min-width: 30%;
+  }
 </style>
--- src/containers/CardContainer.vue
+++ src/containers/CardContainer.vue
@@ -1,14 +1,7 @@
 <template>
-  <div class="card-wrapper">
-    <div class="columns is-8">
-      <christmas-card class="column is-half"></christmas-card>
-      <christmas-card class="column"></christmas-card>
-      <christmas-card class="column"></christmas-card>
-    </div>
-    <div class="columns is-8">
-      <christmas-card class="column"></christmas-card>
-      <christmas-card class="column"></christmas-card>
-      <christmas-card class="column is-half"></christmas-card>
+  <div class="card-container">
+    <div class="wrapper">
+      <christmas-card v-for="card in cards" :key="card.key" :card="card"></christmas-card>
     </div>
   </div>
 </template>
@@ -20,12 +13,65 @@ export default {
   name: "cardContainer",
   components: {
     ChristmasCard
+  },
+  data () {
+    return {
+      cards: [{
+        key: 1,
+        img: require('../assets/card1.jpg'),
+        title: 'Ông già Noel',
+        subtitle: '@Noel',
+        content: 'Merry Christmas',
+        tag: ['@son.ht','#snow', '#winter'],
+        time:'10:07 PM - 25 Dec 2018'
+      },{
+        key: 2,
+        img: require('../assets/card2.png'),
+        title: 'Ngôi nhà tuyết',
+        subtitle: '@Home',
+        content: 'Merry Christmas',
+        tag: ['@son.ht','#home', '#noel'],
+        time:'10:07 PM - 25 Dec 2018'
+      },{
+        key: 3,
+        img: require('../assets/card3.png'),
+        title: 'Ông già Noel',
+        subtitle: '@Noel',
+        content: 'Merry Christmas',
+        tag: ['@son.ht','#snow', '#winter'],
+        time:'10:07 PM - 25 Dec 2018'
+      },{
+        key: 4,
+        img: require('../assets/card4.jpg'),
+        title: 'Ngôi nhà tuyết',
+        subtitle: '@Home',
+        content: 'Merry Christmas',
+        tag: ['@son.ht','#home', '#noel'],
+        time:'10:07 PM - 25 Dec 2018'
+      },{
+        key: 5,
+        img: require('../assets/card5.gif'),
+        title: 'Ông già Noel',
+        subtitle: '@Noel',
+        content: 'Merry Christmas',
+        tag: ['@son.ht','#snow', '#winter'],
+        time:'10:07 PM - 25 Dec 2018'
+      },{
+        key: 6,
+        img: require('../assets/card6.gif'),
+        title: 'Ngôi nhà tuyết',
+        subtitle: '@Home',
+        content: 'Merry Christmas',
+        tag: ['@son.ht','#home', '#noel'],
+        time:'10:07 PM - 25 Dec 2018'
+      }]
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-  .card-wrapper {
+  .card-container {
     width: 70%;
     padding: 50px;
     background: white;
@@ -33,6 +79,11 @@ export default {
     border-radius: 4px;
     z-index: 2;
     margin-top: 40px;
+
+    .wrapper {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-around;
+    }
   }
 </style>
-
